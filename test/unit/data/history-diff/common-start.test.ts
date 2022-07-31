dab82f44efcbab85862b60e55891bee2c2f1ee7d
wmxpy <wm@mengw.io>
2022-07-31 14:18:27 -0500

    add common start unit tests

--- test/unit/data/history-diff/common-start.test.ts
+++ test/unit/data/history-diff/common-start.test.ts
@@ -7,7 +7,7 @@
 
 import { expect } from "chai";
 import * as Chance from "chance";
-import { createMiphaMarkdownBlock } from "../../../../src";
+import { createMiphaMarkdownBlock, createPreciseMiphaMarkdownBlock } from "../../../../src";
 import { findHistoryBlockCommonStart } from "../../../../src/data/history-diff/common-start";
 
 describe('Given [CommonStart] Methods', (): void => {
@@ -17,6 +17,7 @@ describe('Given [CommonStart] Methods', (): void => {
     it('should be able to find common start for single block', async (): Promise<void> => {
 
         const first: string = chance.string();
+
         const firstBlock = createMiphaMarkdownBlock(first);
 
         const firstHistory = firstBlock.histories[0];
@@ -29,6 +30,28 @@ describe('Given [CommonStart] Methods', (): void => {
             commonStart: firstHistory,
             bestBlock: firstBlock,
             appliedBlocks: [firstBlock],
+            unappliedBlocks: [],
+        });
+    });
+
+    it('should be able to find common start for double same block', async (): Promise<void> => {
+
+        const content: string = chance.string();
+
+        const firstBlock = createMiphaMarkdownBlock(content);
+        const secondBlock = createPreciseMiphaMarkdownBlock(firstBlock.identifier, content);
+
+        const firstHistory = firstBlock.histories[0];
+
+        const result = findHistoryBlockCommonStart([
+            firstBlock,
+            secondBlock,
+        ]);
+
+        expect(result).to.be.deep.equal({
+            commonStart: firstHistory,
+            bestBlock: firstBlock,
+            appliedBlocks: [firstBlock, secondBlock],
             unappliedBlocks: [],
         });
     });
