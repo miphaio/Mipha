a179b1cec3bb8b5eb0aaf1b238c831f550082986
wmxpy <wm@mengw.io>
2022-08-13 17:39:08 -0500

    init module domain

---
+++ src/module/abstract/pattern.ts
@@ -0,0 +1,9 @@
+/**
+ * @author WMXPY
+ * @namespace Module_Abstract
+ * @description Abstract
+ */
+
+export abstract class MiphaModuleAbstract {
+
+}
---
+++ src/module/export.ts
@@ -0,0 +1,5 @@
+/**
+ * @author WMXPY
+ * @namespace Module
+ * @description Export
+ */
