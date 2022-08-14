c8b6f2711ca2334bd7bcdb8174e84763d09eee22
wmxpy <wm@mengw.io>
2022-08-13 23:02:45 -0500

    add excuter with amrked

--- src/executer/executer.ts
+++ src/executer/executer.ts
@@ -4,7 +4,19 @@
  * @description Executer
  */
 
+import { Sandbox } from "@sudoo/marked";
+
 // Public
 export class MiphaExecuter {
 
+    public static fromScratch(): MiphaExecuter {
+        return new MiphaExecuter();
+    }
+
+    private readonly _marked: Sandbox;
+
+    private constructor() {
+
+        this._marked = Sandbox.create();
+    }
 }
