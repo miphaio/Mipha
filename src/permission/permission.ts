80d75ff7ecf472ce8add8e99c5bd6fe453c52d95
wmxpy <wm@mengw.io>
2022-08-20 12:37:35 -0500

    add permission scope

--- src/permission/permission.ts
+++ src/permission/permission.ts
@@ -4,6 +4,24 @@
  * @description Permission
  */
 
+// Public
 export class MiphaPermission {
 
+    public static fromIdentifier(identifier: string, scope: string[]): MiphaPermission {
+
+        return new MiphaPermission(identifier, scope);
+    }
+
+    private readonly _identifier: string;
+    private readonly _scope: Set<string>;
+
+    private constructor(identifier: string, scope: string[]) {
+
+        this._identifier = identifier;
+        this._scope = new Set<string>(scope);
+    }
+
+    public get identifier(): string {
+        return this._identifier;
+    }
 }
