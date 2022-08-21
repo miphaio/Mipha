ef36ad52ce2968e2df24b348d54628cb9bc281de
wmxpy <wm@mengw.io>
2022-08-21 16:17:04 -0500

    init integ test

--- test/integration/get-data/setup/get-data.ts
+++ test/integration/get-data/setup/get-data.ts
@@ -5,9 +5,20 @@
  * @override Setup
  */
 
-import { MiphaExecuter, MiphaModule } from "../../../../src";
+import { MiphaExecuter, MiphaExecuterAdditionalArgument, MiphaModule } from "../../../../src";
 
 const getDataFirstModule = MiphaModule.fromScratch('integration.get-data.first');
+getDataFirstModule.provide('first', (arg: MiphaExecuterAdditionalArgument) => {
+
+    arg.permissionController.assert(
+        'integration.get-data.first',
+        'value',
+        'first',
+    );
+
+    return 'first';
+});
+
 const getDataSecondModule = MiphaModule.fromScratch('integration.get-data.second');
 
 export const getDataIntegrationExecuter = MiphaExecuter.fromModulesAndRecipes(
---
+++ test/integration/get-data/test/permission.test.ts
@@ -0,0 +1,32 @@
+/**
+ * @author WMXPY
+ * @namespace GetData
+ * @description Permission
+ * @override Integration
+ */
+
+import { MarkedResult } from "@sudoo/marked";
+import { expect } from "chai";
+import * as Chance from "chance";
+import { MiphaScript } from "../../../../src";
+import { getDataIntegrationExecuter } from "../setup/get-data";
+
+describe('Given (Permission) cases for (GetData) Integration test setup', (): void => {
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const chance: Chance.Chance = new Chance('integration-get-data-permission');
+
+    it('should be able to execute module with permission', async (): Promise<void> => {
+
+        const script: MiphaScript = MiphaScript.fromCode(
+            `import {first} from "integration.get-data.first";export default first()`
+        );
+
+        const result: MarkedResult = await getDataIntegrationExecuter.mountAndExecute(
+            script,
+            [],
+        );
+
+        expect(result).to.be.equal('first');
+    });
+});
