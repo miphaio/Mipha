03096e390dce77a63e9745a7f90311bfb8a6fabd
wmxpy <wm@mengw.io>
2022-10-23 02:43:41 -0500

    add init grid, add jss package

---
+++ src/grid/grid.tsx
@@ -0,0 +1,17 @@
+/**
+ * @author WMXPY
+ * @namespace Grid
+ * @description Grid
+ */
+
+import * as React from "react";
+
+export type MiphaGridProps = {
+};
+
+export const MiphaGrid: React.FC<MiphaGridProps> = (props: MiphaGridProps) => {
+
+    return <div>
+        Test
+    </div>;
+};
---
+++ src/grid/style/grid.style.ts
@@ -0,0 +1,5 @@
+/**
+ * @author WMXPY
+ * @namespace Grid_Style
+ * @description Grid
+ */
